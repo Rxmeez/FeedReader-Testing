4c0c9eba11e6e6e985510c40773e96ef14afa3c3
Rameez Khan <Rameez@Rameezs-MacBook-Pro.local>
2017-02-17 06:35:14 +0000

    Run through JSBeautifer

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,108 +1,95 @@
-/* feedreader.js
- *
- * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
- */
+$(function() {
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
-$(function(){
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
-    describe('RSS Feeds', function(){
+    describe('RSS Feeds', function() {
 
-        it('are defined', function(){
+        it('are defined', function() {
             expect(allFeeds).toBeDefined(); // allFeeds is defined
             expect(allFeeds.length).not.toBe(0); //allFeeds Array is not empty
         });
 
 
-         it('url are defined and not empty', function(){
-           for (i = 0; i < allFeeds.length; i = i + 1){ // to go through allFeeds array to check for url
-             expect(allFeeds[i].url).toBeDefined(); // url is defined
-             expect(allFeeds[i].url).not.toBeNull(); // url is not null
-             expect(allFeeds[i].url).not.toBe(""); // url is not empty string
-           }
-         });
+        it('url are defined and not empty', function() {
+            for (i = 0; i < allFeeds.length; i = i + 1) { // to go through allFeeds array to check for url
+                expect(allFeeds[i].url).toBeDefined(); // url is defined
+                expect(allFeeds[i].url).not.toBeNull(); // url is not null
+                expect(allFeeds[i].url).not.toBe(""); // url is not empty string
+            }
+        });
 
 
-         it('name are defined and not empty', function(){
-           for (i = 0; i < allFeeds.length; i = i + 1){ // to go through allFeeds array to check for name
-             expect(allFeeds[i].name).toBeDefined(); // name is defined
-             expect(allFeeds[i].name).not.toBeNull(); // name is not null
-             expect(allFeeds[i].name).not.toBe(""); // name is not empty string
-           }
-         });
+        it('name are defined and not empty', function() {
+            for (i = 0; i < allFeeds.length; i = i + 1) { // to go through allFeeds array to check for name
+                expect(allFeeds[i].name).toBeDefined(); // name is defined
+                expect(allFeeds[i].name).not.toBeNull(); // name is not null
+                expect(allFeeds[i].name).not.toBe(""); // name is not empty string
+            }
+        });
 
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-    describe("The menu", function(){
+    describe("The menu", function() {
 
-         it('is hidden by default', function(){
-           expect($('body').hasClass('menu-hidden')).toEqual(true); // default menu-hidden is hidden hence should be true
-         });
+        it('is hidden by default', function() {
+            expect($('body').hasClass('menu-hidden')).toEqual(true); // default menu-hidden is hidden hence should be true
+        });
 
-          it("shows and hides when clicked on", function(){
+        it("shows and hides when clicked on", function() {
 
             $('.menu-icon-link').click(); // Intially its hidden so trigger click to open menu
             expect($('body').hasClass('menu-hidden')).toEqual(false); // if menu is open toEqual false
 
             $('.menu-icon-link').click(); //state of menu is open hence trigger click to close menu
             expect($('body').hasClass('menu-hidden')).toEqual(true); // if menu is closed toEqual true
-          });
-
-          });
-
-    /* TODO: Write a new test suite named "Initial Entries" */
-    describe("Initial Entries", function(){
-
-      // beforeEach to wait for async calls to finish
-          beforeEach(function(done){
-          loadFeed(0, function(){
-              done();
-          });
         });
 
-         it("loads atleast one entry within the feed container", function(){
-           expect($('.entry').length).not.toEqual(0); // if there is an entry it will not equal 0
-         });
+    });
 
-         });
+    /* TODO: Write a new test suite named "Initial Entries" */
+    describe("Initial Entries", function() {
+
+        // beforeEach to wait for async calls to finish
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        it("loads atleast one entry within the feed container", function() {
+            expect($('.entry').length).not.toEqual(0); // if there is an entry it will not equal 0
+        });
+
+    });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
-    describe("New Feed Selection", function(){
+    describe("New Feed Selection", function() {
 
-         // variable feeds that will be used for comparison
-         var firstFeed;
-         var secondFeed;
+        // variable feeds that will be used for comparison
+        var firstFeed;
+        var secondFeed;
 
-         // beforeEach to wait for async calls to finish
-         beforeEach(function(done){
-             loadFeed(1, function(){
-                 firstFeed = $('.feed').html();
-                 loadFeed(2, function(){
-                     done();
-                 });
-             });
-          });
+        // beforeEach to wait for async calls to finish
+        beforeEach(function(done) {
+            loadFeed(1, function() {
+                firstFeed = $('.feed').html();
+                loadFeed(2, function() {
+                    done();
+                });
+            });
+        });
 
-         // afterEach to reload first entry
-         afterEach(function(){
-             loadFeed(0);
-         });
+        // afterEach to reload first entry
+        afterEach(function() {
+            loadFeed(0);
+        });
 
-          it('displays feed content changes on menu select', function(){
-             expect(firstFeed).toBeDefined();  //firstFeed is defined
-             secondFeed = $('.feed').html();  // secondFeed is loaded
-             expect(secondFeed).toBeDefined(); // secondFeed is defined
-             expect(firstFeed).not.toEqual(secondFeed); // firstFeed not.toEqual secondFeed as new content should have loaded
-          });
+        it('displays feed content changes on menu select', function() {
+            expect(firstFeed).toBeDefined(); //firstFeed is defined
+            secondFeed = $('.feed').html(); // secondFeed is loaded
+            expect(secondFeed).toBeDefined(); // secondFeed is defined
+            expect(firstFeed).not.toEqual(secondFeed); // firstFeed not.toEqual secondFeed as new content should have loaded
+        });
 
-         });
+    });
 }());
